d2a8209b50920ab6e1a0c27717f10ec39e960fbc
jcurtiss <jcurtiss@iastate.edu>
2013-10-31 11:47:19 -0500

    new package refactor except for deleted files

---
+++ CyBite App/src/com/se319/cybite/NearByActivity.java
@@ -0,0 +1,24 @@
+package com.se319.cybite;
+
+import com.example.cybite.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class NearByActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_near_by);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.near_by, menu);
+		return true;
+	}
+
+}
